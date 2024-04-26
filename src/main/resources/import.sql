f1728b980dcd2fc282e4dd6dbb12cfaa4a804951
otaviomalvess <otaviomalvess@gmail.com>
2024-04-26 17:01:37 -0300

    refactor: update import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,16 +5,16 @@ VALUES
         (2, 'Telefone'),
         (3, 'Chave Aleatória');
 
-INSERT INTO directory(pkey, pkey_type, cpf, owner, creation_date)
+INSERT INTO directory(pkey, pkey_type, cpf, owner, creation_date, bank, branch, account)
 VALUES 
-        ('mail@mail.com', 1, '11111111101', 'Ana', NOW()),
-        ('+5561123456789', 2, '22222222201', 'Bob', NOW()),
-        ('11111111101', 0, '11111111101', 'Ana', NOW());
+        ('mail@mail.com', 1, '11111111101', 'Ana', NOW(), 000, 0, 0),
+        ('+5561123456789', 2, '22222222201', 'Bob', NOW(), 000, 0, 0),
+        ('11111111101', 0, '11111111101', 'Ana', NOW(), 000, 0, 0);
 
 INSERT INTO account(cpf, name, bank, branch, account, balance)
 VALUES
-        ('11111111101', 'Ana', '', '', '', 100.0),
-        ('22222222201', 'Bob', '', '', '', 100.0);
+        ('11111111101', 'Ana', 000, 0, 0, 100.0),
+        ('22222222201', 'Bob', 000, 0, 0, 100.0);
 
 -- INSERT INTO pix_request(cpf, value, resolved)
 -- VALUES 
