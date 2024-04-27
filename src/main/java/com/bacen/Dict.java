fea2231cc96af7e73dd6fc71b3f320b6c733b992
otaviomalvess <otaviomalvess@gmail.com>
2024-04-27 18:41:58 -0300

    remove: deprecated consult existent keys method

--- src/main/java/com/bacen/Dict.java
+++ src/main/java/com/bacen/Dict.java
@@ -11,33 +11,6 @@ public class Dict {
     @Inject
     private BacenDB db;
 
-    @Inject
-    private Logger logger;
-    
-    /**
-     * Checks if there are any {@code Entry} matching the given keys.
-     *
-     * @param keys array of {@code Entry} keys.
-     * @return a {@code LinkedHashMap} containing the array of keys and an array of booleans. 
-     */
-    public LinkedHashMap<String, Object> consultExistentKeys(final String[] keys) {
-        if (keys == null || keys.length == 0) {
-            return null;
-        }
-
-        final BacenEntry[] entries = db.getEntries(keys);
-        final boolean[] existent = new boolean[keys.length];
-        for (int i = 0; i < keys.length; i++) {
-            existent[i] = entries[i] != null;
-        }
-        
-        final LinkedHashMap<String, Object> r = new LinkedHashMap<String, Object>();
-        r.put("keys", keys);
-        r.put("entries", existent);
-
-        return r;
-    }
-
     /**
      * Checks if there are any {@code Entry} matching the given keys.
      *
