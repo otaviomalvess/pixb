6eee1201677ed79baadad596bbe4ff23d1a6e673
otaviomalvess <otaviomalvess@gmail.com>
2024-04-27 18:41:27 -0300

    refactor: remove commented method

--- src/main/java/com/bacen/DictResource.java
+++ src/main/java/com/bacen/DictResource.java
@@ -31,31 +31,4 @@ public class DictResource {
             return Response.serverError().build();
         }
     }
-
-    // @POST
-    // @Transactional
-    // public Response pix(LinkedHashMap<String, String> json) {
-    //     try {
-    //         Pix p = new ObjectMapper().convertValue(json, Pix.class);
-    //         Account from = Account.findById(p.accountFrom);
-    //         Account to = Account.findById(p.accountTo);
-
-    //         if (from.balance - p.value < 0.0) {
-    //             return Response.ok("Senter doesn't have enough balance.").build();
-    //         }
-            
-    //         from.balance -= p.value;
-    //         to.balance += p.value;
-    //         p.date = new Date();
-
-    //         from.persist();
-    //         to.persist();
-    //         p.persist();
-            
-    //         return Response.ok().build();
-
-    //     } catch (Exception e) {
-    //         return Response.serverError().build();
-    //     }
-    // }
 }
