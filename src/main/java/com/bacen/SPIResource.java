6eae6f5256f091c685af19d07bc641002b9a5775
otaviomalvess <otaviomalvess@gmail.com>
2024-04-27 18:51:30 -0300

    feat: import response produces default media type

--- src/main/java/com/bacen/SPIResource.java
+++ src/main/java/com/bacen/SPIResource.java
@@ -6,10 +6,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/bacen/spi/request")
 public class SPIResource {
 
