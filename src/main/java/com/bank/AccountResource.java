9191b3c84c424bd61982117f6790e41a417145cb
otaviomalvess <otaviomalvess@gmail.com>
2024-04-27 18:21:22 -0300

    refactor: change class endpoints path

--- src/main/java/com/bank/AccountResource.java
+++ src/main/java/com/bank/AccountResource.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/pixb/account/{id}")
+@Path("/bank/account")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AccountResource {
