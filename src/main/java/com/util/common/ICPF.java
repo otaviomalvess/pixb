ee7647b233fb2ba4d9d858ae29d3dd073add8092
otaviomalvess <otaviomalvess@gmail.com>
2024-05-02 00:00:26 -0300

    feat: implement interfaces

---
+++ src/main/java/com/util/common/ICPF.java
@@ -0,0 +1,12 @@
+package com.util.common;
+
+public interface ICPF {
+    
+    /**
+     * Returns the CPF value.
+     * 
+     * @return The CPF value.
+     * @see CPF
+     */
+    public String getCPF();
+}
