b91e361ba9603a9934f2cd5ad2e0ea15ca9b0285
otaviomalvess <otaviomalvess@gmail.com>
2024-05-02 01:01:47 -0300

    refactor: set enum accessibility

--- src/main/java/com/bacen/model/KeyType.java
+++ src/main/java/com/bacen/model/KeyType.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 @Table(name = "key_type")
 public class KeyType extends PanacheEntityBase {
     
-    enum Types { CPF, EMAIL, PHONE, RANDOM }
+    public enum Types { CPF, EMAIL, PHONE, RANDOM }
 
     @Id
     @Column(name = "id")
